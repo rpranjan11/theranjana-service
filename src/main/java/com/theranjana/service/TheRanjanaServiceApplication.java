7dd50840e6eb617e9d8a2d5456a237019f4f131e
Ranjan [란잔] <ranjan.rp@timbel.net>
2025-01-06 11:48:14 +0900

    main - code setup

---
+++ src/main/java/com/theranjana/service/TheRanjanaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.theranjana.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRanjanaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheRanjanaServiceApplication.class, args);
+    }
+
+}
